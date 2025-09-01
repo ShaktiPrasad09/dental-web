2478e0757a0395597f67c12b87b125a591cba44b
Shakti Prasad Mishra <112119739+ShaktiPrasad09@users.noreply.github.com>
2025-09-01 16:02:27 +0530

    footer added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header';
-import { LandingComponent } from './components/landing/landing';
+import { FooterComponent } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
